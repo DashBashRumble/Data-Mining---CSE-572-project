0cd0b16873776b3f0549751bc5a262c013b21d55
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-11-09 19:59:12 -0700

    Code refactoring

--- classification.py
+++ classification.py
@@ -25,6 +25,10 @@ class Classification:
         self.y_test = None
         self.test_train_split()
         self.y_pred = None
+        self.accuracy = None
+        self.f1_score = None
+        self.recall = None
+        self.precision = None
 
     def test_train_split(self):
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.x, self.y, test_size=0.3,
@@ -58,10 +62,14 @@ class Classification:
 
     def get_metrics(self):
         print('classifier:', self.classifier_name)
-        print('Accuracy score', accuracy_score(self.y_test, self.y_pred))
-        print('Precision', precision_score(self.y_test, self.y_pred))
-        print('Recall', recall_score(self.y_test, self.y_pred))
-        print('F1 score', f1_score(self.y_test, self.y_pred))
+        self.accuracy = accuracy_score(self.y_test, self.y_pred)
+        print('Accuracy score', self.accuracy)
+        self.precision = precision_score(self.y_test, self.y_pred)
+        print('Precision', self.precision)
+        self.recall = recall_score(self.y_test, self.y_pred)
+        print('Recall', self.recall)
+        self.f1_score = f1_score(self.y_test, self.y_pred)
+        print('F1 score', self.f1_score)
         # print('confusion matrix')
         # print(confusion_matrix(self.y_test, self.y_pred))
         # print(classification_report(self.y_test, self.y_pred))
